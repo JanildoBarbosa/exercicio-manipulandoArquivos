421c86ab821206120038252781480434aa31b49c
Janildo Barbosa <janildofbabosa@gmail.com>
2022-12-28 14:31:57 -0300

    Using classes FileReader and BufferedReader

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,51 +1,32 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = null;
 		
-		Scanner sc = new Scanner(System.in);
+		String path = "c:\\projetos\\modulo14\\vendas.csv";
 		
-		System.out.println("Enter a file path:");
-		String strPath = "c:\\projetos\\modulo14\\vendas.csv";
-		String[] vect = new String[4];
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(strPath))){
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
 			String line = br.readLine();
-			vect = line.split(",");
 			
-			for (int i = 0; i <= 3; i++) {
-				if (line != null) {
-					System.out.println(line);
-					line = br.readLine();
-			
-				}
-			}			
-		}
-		catch (IOException e){
-			e.printStackTrace();
-		}
-		finally {
-			sc.close();
+			while (line != null) {
+				System.out.println(line);
+				line = br.readLine();
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR: " + e.getMessage());
 		}
 
-		for (String string : vect) {
-			System.out.println(string);
-		}
-
-		File file = new File(strPath);
-		file.listFiles(File::isDirectory);
-
-		
-		boolean sucess = new File(strPath + "\\out").mkdir();
-
-		System.out.println("Directory created successfully: " + sucess);
 	}
 
 }
