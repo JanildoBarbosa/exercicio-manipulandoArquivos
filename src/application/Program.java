029f73e4d9f59ccb915099f7a472f28396a1cef7
Janildo Barbosa <janildofbabosa@gmail.com>
2022-12-29 04:03:41 -0300

    Refactore try block

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Scanner;
@@ -11,22 +13,31 @@ public class Program {
 	public static void main(String[] args) {
 
 		Locale.setDefault(Locale.US);
-		Scanner sc = null;
-		
+
 		String path = "c:\\projetos\\modulo14\\vendas.csv";
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			
+		String path2 = "c:\\projetos\\modulo14\\out.csv";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(path2));
+
 			String line = br.readLine();
-			
+			String[] words = new String[3];
+			words = line.split(",");
+
+			for (String string : words) {
+				System.out.println(string);
+			}
 			while (line != null) {
 				System.out.println(line);
 				line = br.readLine();
+
 			}
+
 		} catch (IOException e) {
 			System.out.println("ERROR: " + e.getMessage());
 		}
+		
+		
 
 	}
-
 }
