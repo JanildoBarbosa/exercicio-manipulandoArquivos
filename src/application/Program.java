65e298ffd547c2bc5f119489e34a8cbc2ed848d3
Janildo Barbosa <janildofbabosa@gmail.com>
2022-12-30 01:40:03 -0300

    End project

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,47 +1,63 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
 
-		String path = "c:\\projetos\\modulo14\\in.txt";
+		String path = "C:\\projetos\\modulo14\\in.csv";
 		
 		List<Product>list = new ArrayList<>();
-
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){			
 			String line = br.readLine();
-
+			
 			while (line != null) {
 				System.out.println(line);
 				
-				String[] words = new String[3];
-				words = line.split(",");
-				String name = words[0];
-				double price = Double.parseDouble(words[1]); 
-				int quantity = Integer.parseInt(words[2]);
+				String[] attributes = new String[3];
+				attributes = line.split(",");
+				
+				String name = attributes[0];
+				Double price = Double.parseDouble(attributes[1]);
+				int quantity = Integer.parseInt(attributes[2]);
 				
 				Product product = new Product(name, price, quantity);
-				list.add(product);
+				list.add(product);				
 				
 				line = br.readLine();
+			}
+			
+			String strOut = "C:\\projetos\\modulo14\\out.csv";
+			
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(strOut))){
+				
+				for (Product product : list) {
+					bw.write(product.getName() + ", " + product.getPrice() * product.getQuantity());
+					bw.newLine();
+				}
 				
 			}
-			
-			for (Product product : list) {
-				System.out.println(product);
+			catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
 			}
-			
+							
 		} 
 		catch (IOException e) {
-			System.out.println("ERROR: " + e.getMessage());
+				System.out.println("Error: " + e.getMessage());
+				
 		}
 	}
 }
